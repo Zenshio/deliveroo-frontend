57982c45413b832e857ce137cd9e568ebd38f4df
Ludovic Six <ludosix@hotmail.com>
2021-11-09 15:13:33 +0100

    Added Footer

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import "./App.css";
 
 import Header from "./components/header/Header";
 import Main from "./components/main/Main";
+import Footer from "./components/footer/Footer";
 
 const App = () => {
   const [data, setData] = useState();
@@ -31,6 +32,7 @@ const App = () => {
         categories={data.categories}
         cart={{ items: cartItems, setItems: setCartItems }}
       />
+      <Footer />
     </div>
   );
 };
---
+++ src/components/footer/Footer.js
@@ -0,0 +1,27 @@
+const Footer = () => {
+  return (
+    <footer className="Footer">
+      <div>
+        Made with React at
+        <a
+          href="http://www.lereacteur.io"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Le Reacteur
+        </a>
+        by
+        <a
+          href="http://www.ludovic-six.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Ludovic Six
+        </a>
+        - 2021
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
